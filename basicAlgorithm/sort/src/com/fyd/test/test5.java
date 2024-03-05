f82d2f504b0ff75292d9e3295630b03b8a0bd954
GanFengYu <patient.fyd@gmail.com>
2024-03-05 16:53:26 +0800

    第k个数（bug）

--- basicAlgorithm/sort/src/com/fyd/test/test5.java
+++ basicAlgorithm/sort/src/com/fyd/test/test5.java
@@ -15,14 +15,25 @@ public class test5 {
         int k = scanner.nextInt();
         int ans = 1; // 1就是最小的字典序，所以初始化为1
         k--; // 直接扣除上面是1的情况
-        while (k>0){
-            // temp表示当前前缀范围有多少数 now 表示当前前缀，next表示下一前缀
+        while (k > 0) {
+            // temp表示当前前缀范围有多少数  now表示当前前缀，next表示下一前缀
             long temp = 0;
             int now = ans;
             int next = ans + 1;
-            while (now<=n){
-                
+            // 满足在n的范围内
+            while (now <= n) {
+                temp += Math.min(n + 1, next) - now;
+                next *= 10;
+                now *= 10;
+            }
+            if (temp<= k){
+                ans+=1;
+                k-=temp;
+            }else {
+                ans*=10;
+                k-=1;
             }
         }
+        System.out.println(ans);
     }
 }
