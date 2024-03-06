db36933f49a64759ba19127748bbd5fe8636b231
GanFengYu <patient.fyd@gmail.com>
2024-03-06 11:38:00 +0800

    Create test7.java

---
+++ basicAlgorithm/sort/src/com/fyd/test/test7.java
@@ -0,0 +1,22 @@
+package com.fyd.test;
+
+import java.util.Scanner;
+
+/**
+ * Author: patient.fyd@gmail.com
+ * Date: 2024/3/6  11:05
+ * Description: 第三大的和
+ */
+public class test7 {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = scn.nextInt();
+        }
+
+
+
+    }
+}
